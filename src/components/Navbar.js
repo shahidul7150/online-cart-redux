651e1d26ee5e8b07a6210245c1f03d2594b36da1
Shahidul Islam <shahidulislam7150@gmail.com>
2022-07-03 13:24:31 +0600

    create navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from "react-router-dom"
 const Navbar = () => {
   return (
-      <nav>
+      <nav className='nav-bar'>
           <Link to="/">
               
       <h2>OnlineShop</h2>
