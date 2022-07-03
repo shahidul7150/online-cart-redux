790ac4180b7156c25d5f206827afe82396e70afb
Shahidul Islam <shahidulislam7150@gmail.com>
2022-07-03 13:00:57 +0600

    redux impletent project init commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={ <Home/>}></Route>
+        <Route path='/cart' element={ <Cart/>}></Route>
+      </Routes>
     </div>
   );
 }
