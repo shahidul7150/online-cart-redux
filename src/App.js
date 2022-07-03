91e53fd608e88a52164944c26145b8e79fbb39fb
Shahidul Islam <shahidulislam7150@gmail.com>
2022-07-03 13:46:36 +0600

    create not found page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Home/>}></Route>
         <Route path='/cart' element={ <Cart/>}></Route>
+        <Route path='*' element={ <NotFound/>}></Route>
       </Routes>
     </div>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='notFound'>
+           
+            <h2>Not Found</h2>
+            <h1>404</h1>
+          
+        </div>
+    );
+};
+
+export default NotFound;
