57b50892a8a9fa46be2d93a9671263559e44ddcb
Shahidul Islam <shahidulislam7150@gmail.com>
2022-07-04 16:17:53 +0600

    rtk query add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useGetAllProductsQuery } from '../features/productsApi';
 
 const Home = () => {
+    const { data, error, isLoading } = useGetAllProductsQuery()
+    console.log("Api ",isLoading);
+    console.log("Data ",data);
     return (
         <div>
             <h2>Home</h2>
+            <p>{data.length}</p>
+            <p>{data[0].name}</p>
+            <img src={data[0].image} alt="" />
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {
-  BrowserRouter
-} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import productsReducer, { productsFetch } from './features/productsSlice';
+import { productsApi } from './features/productsApi';
+
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    [productsApi.reducerPath]:productsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productsApi.middleware),
+  
+})
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
-  <React.StrictMode>
-    <App />
+  <Provider store={store}>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
     </React.StrictMode>
     </BrowserRouter>
+    </Provider>
 );
-
-
